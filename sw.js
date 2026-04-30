195cb7c3e7d8eec1399485a63939b73461ef6ecd
igorkaucic <igor.kaucic@agilosit.com>
2026-04-30 15:10:06 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ﻿// GEA Service Worker â€” Auto-Update Engine
-// VERSION: 12.9
-const CACHE_VERSION = 'gea-v12.9';
+// VERSION: 13.0
+const CACHE_VERSION = 'gea-v13.0';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 
 // Assets to pre-cache on install

5f85dfab1d965a8aff358624a7eb2bd65d8804f4
igorkaucic <igor.kaucic@agilosit.com>
2026-05-01 17:22:23 +0200

    15.0 - deploy

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 ﻿// GEA Service Worker â€” Auto-Update Engine
-// VERSION: 14.9
-const CACHE_VERSION = 'gea-v14.9';
+// VERSION: 15.0
+const CACHE_VERSION = 'gea-v15.0';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 
 // Assets to pre-cache on install
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-﻿export const APP_VERSION = '14.9';
+﻿export const APP_VERSION = '15.0';
